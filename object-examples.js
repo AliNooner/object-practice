65ded109ce685a9d6490131a4e5e296ab5209abc
Ali Nooner <alinooner@gmail.com>
2021-10-20 21:46:56 -0500

    Add customer model

--- object-examples.js
+++ object-examples.js
@@ -1,4 +1,4 @@
-//model for Jeep
+//model for car
 var Jeep = {
   model : "Cherokee",
   color : "white",
@@ -13,4 +13,13 @@ var Elephant = {
   size : "large",
   color : "grey",
   predominantFeatures : "trunk", "hooves", "great memory"
+}
+
+
+//model for customer
+
+var = Customer = {
+  name : "Ali",
+  location : "Wisconsin",
+  payingMember : true
 }
