6b31f9e8ef8246978dbfff598a258194d73b9cd3
Ali Nooner <alinooner@gmail.com>
2021-10-20 21:42:48 -0500

    Add animal model

--- object-examples.js
+++ object-examples.js
@@ -1,2 +1,16 @@
 //model for Jeep
-var carParts = ["tires", "windows", "engine", "body"]
+var Jeep = {
+  model : "Cherokee",
+  color : "white",
+  numberOfTires : 4,
+  batteryIsCharged : true,
+  dollarPrice : 40000
+}
+
+//model for animal
+
+var Elephant = {
+  size : "large",
+  color : "grey",
+  predominantFeatures : "trunk", "hooves", "great memory"
+}
